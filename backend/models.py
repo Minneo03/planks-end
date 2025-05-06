f5abbe8e0786524550367249206ef39541bce1f4
Ryan Minneo <ryanminneo03@gmail.com>
2025-05-06 19:36:15 -0400

    Refresh
    Switching from Angular + Flask to just Flask framework.

--- backend/models.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
-
-class Ticket(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(50))
-    price = db.Column(db.Float)
