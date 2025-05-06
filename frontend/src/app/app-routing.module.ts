3532ce7baae640285600154193b1d3daa4798659
Ryan Minneo <ryanminneo03@gmail.com>
2025-05-06 14:55:17 -0400

    Project Folder Organization

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,6 @@
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'amusements', component: AmusementsComponent },
+  { path: 'tickets', component: TicketsComponent },
+  { path: 'about', component: AboutComponent }
+];
