383cc795ea3083a77085926aed9f9c9cc90faac0
Ryan Minneo <ryanminneo03@gmail.com>
2025-05-06 21:39:58 -0400

    Remaking the Backend
    Remade the database and fixed the routing for the Flask framework. We have ditched angular, as angular seems to be causing issues on mac systems for whatever reason.

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask
-from flask_cors import CORS
-from routes.amusements import amusements_bp
-from routes.tickets import tickets_bp
+from flask import Flask, render_template
 
 app = Flask(__name__)
-CORS(app)
 
-app.register_blueprint(amusements_bp, url_prefix='/api/amusements')
-app.register_blueprint(tickets_bp, url_prefix='/api/tickets')
+@app.route('/')
+def home():
+    return render_template('pages/homePage.html')
+
+@app.route('/amusements')
+def amusements():
+    return render_template('pages/amusements.html')
+
+@app.route('/ticketing')
+def ticketing():
+    return render_template('pages/ticketing.html')
+
+@app.route('/aboutUs')
+def aboutUs():
+    return render_template('pages/aboutUs.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
